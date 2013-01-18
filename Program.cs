1dc6a9f3fe7d90fcc06183b3a2fa3a60e7680204
Maksim Kozhukh <mkozhukh@ya.ru>
2013-01-18 09:51:37 +0300

    [add] initial version

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace BrowserLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                string path = args[0];
+                string folder = System.IO.Path.GetDirectoryName(path);
+                while (folder != null){
+                    string config_file = folder + "\\"+".webroot";
+                    if (System.IO.File.Exists(config_file)){
+                        string server_path = System.IO.File.ReadAllText(config_file).Trim();
+                        string http_path = path.Replace(folder, server_path);
+                        Process.Start(http_path);
+                        break;
+                    }
+
+                    System.IO.DirectoryInfo info = System.IO.Directory.GetParent(folder);
+                    if (info != null)
+                        folder = info.FullName;
+                    else
+                        folder = null;
+                }
+            }
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 browser-launcher
 ================
 
-Translate file paths to http urls
+Translates file paths to http urls, so you can open http files with valid urls instead of file://protocol.
+
+
+### How to use
+
+
+- shift+right click on html (php or any other web format) file
+- select "Open with" command
+- select BrowserLauncher as default program for this file type
+- go to webserver root and create there ".webroot" file, with http address of server ( check .webroot.sample in the project ). 
+
+That is it! Now you can open any file through explorer ( or total command, or any other program) and it will be opened in browser by valid http url, instead of ugly file:// protocol. 
+
+You can have different .webroot files for different folders, so files from each folder can be configured to different web servers. 
+
